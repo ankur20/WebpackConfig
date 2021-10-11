e396427ac330e98fa52f509b47cb57274506df88
Ankur Sharma <anksharm13@publicisgroupe.net>
2021-10-11 09:35:43 +0100

    Configure babel for transpilation

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   mode: "development",
   devtool: false,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
   devServer: {
     static: "./dist", // in V3 (old webpack cli) you can use contentBase instead of static, live reloading.
   },
